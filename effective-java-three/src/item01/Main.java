95eed2059990d4948c638cff55a6f3727a5e1810
rongjie Han <hanrongjie2011@gmail.com>
2020-09-11 17:25:13 +0800

    Integer

--- effective-java-three/src/item01/Main.java
+++ effective-java-three/src/item01/Main.java
@@ -1,13 +1,41 @@
 package item01;
 
+import jdk.internal.util.EnvUtils;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
+ * Item 1: Consider static factory methods instead of constructors
+ * 静态方法替代构造器。
+ *
+ * @see java.util.ServiceLoader
  * @author hanrongjie
  * @date 2020/9/11
  */
 public class Main {
 
+    /**
+     * @see java.lang.Integer.IntegerCache
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //eg.  high 取决于 -D java.lang.Integer.IntegerCache.high=2048 配置
+        int a = Integer.valueOf("10000");
+        int b = Integer.valueOf("10000");
+        //true
+        System.out.println(a == b);
+        Integer a0  = Integer.valueOf("10000");
+        Integer b0 = Integer.valueOf("10000");
+        //false
+        System.out.println(a0 == b0);
+        BigInteger.TEN
+
+
+
+
     }
 }
 
