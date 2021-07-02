d1ec80aa52ca60e59c53da43c65722fbbbaabace
100Mbps <hanrongjie@hotmail.com>
2021-07-02 21:11:33 +0800

    refactor

---
+++ java-se-api/src/com/dgyt/seapi/net/test/MultiplePort.java
@@ -0,0 +1,17 @@
+package com.dgyt.seapi.net.test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiplePort {
+    public static void main(String[] args) throws IOException {
+
+
+        ServerSocket ss1 = new ServerSocket(65536);
+
+
+
+
+    }
+}
